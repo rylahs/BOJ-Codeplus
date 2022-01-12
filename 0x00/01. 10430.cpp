57a95e2065d3f06f1c02aeb3bb4883f89ec268a9
Chan ho Moon <minsonyo@naver.com>
2022-01-12 13:47:36 +0900

    01. 10430 나머지 solved
    01. 10430 나머지 solved

---
+++ 0x00/01. 10430.cpp
@@ -0,0 +1,25 @@
+// Code Plus BasicA
+// 02. Math
+// Written by Rylah
+// Written Date : 2022.01.12
+// https://minteul.tistory.com/
+// https://www.acmicpc.net/source/37432221
+
+// 10430. ³ª¸ÓÁö
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << (a + b) % c << '\n';
+	cout << ((a % c) + (b % c)) % c << '\n';
+	cout << (a * b) % c << '\n'; 
+	cout << ((a % c) * (b % c)) % c << '\n';
+
+	return 0;
+}
